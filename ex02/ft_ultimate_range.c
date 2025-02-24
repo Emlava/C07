658af947b73a56b1cd87bda3460091f1580b1615
Emlava <emiliolaravargas@hotmail.com>
2025-02-24 19:16:32 +0100

    added 02

--- ex02/ft_ultimate_range.c
+++ ex02/ft_ultimate_range.c
@@ -16,34 +16,34 @@ int	ft_ultimate_range(int **range, int min, int max)
 {
 	int	i;
 	int	n;
-	
+
 	if (min >= max)
 	{
 		*range = NULL;
 		return (0);
 	}
-	*int_array = (int *)malloc((max - min) * sizeof(int));
+	*range = (int *)malloc((max - min) * sizeof(int));
 	if (*range == NULL)
 		return (-1);
 	i = 0;
 	n = min;
 	while (i < max - min)
 	{
-		*range[i] = n;
+		(*range)[i] = n;
 		i++;
 		n++;
 	}
-	return (n - min);
+	return (max - min);
 }
 
-#include <stdio.h>
+/*#include <stdio.h>
 
 int	main()
 {
 	int min = 5;
-	int max = 10;
-	int **range;
+	int max = 12;
+	int *range = NULL;
 
-	printf("Size of range = %d\n", ft_ultimate_range(range, min, max));
+	printf("Size of range = %d\n", ft_ultimate_range(&range, min, max));
 	return (0);
-}
+}*/
